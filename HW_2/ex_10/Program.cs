afae0a46e2fc494458e6e5fbe5a6a89ca87de38d
Mariia <MK193@yandex.ru>
2022-09-05 11:56:36 +0300

    Ex_10 done and check

---
+++ HW_2/ex_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Трехзначное число...");
+void SecondNumber ()
+{
+int x = new Random().Next(100,1000);;
+Console.WriteLine(x);
+int y = (x%100)/10;
+Console.WriteLine("Вторая цифра этого числа " +y);
+}
+SecondNumber ();
