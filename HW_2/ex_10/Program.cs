2a551ba132896ff10e211d0481d0460fa35cf2bc
Mariia <MK193@yandex.ru>
2022-09-05 15:24:45 +0300

    ex_10 done and check

--- HW_2/ex_10/Program.cs
+++ HW_2/ex_10/Program.cs
@@ -1,14 +1,13 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 Console.WriteLine("Введите трехзначное число...");
-void SecondNumber ();
-{
-    int x = Convert.ToInt32(Console.ReadLine());
-    if (х>99)
+void ThirdNumber ()
+{ int x = Convert.ToInt32 (Console.ReadLine());
+    if (x>99 && x<1000)
     {
-        int y = (x%100)/10;
-        Console.WriteLine("Вторая цифра этого числа " +y);
-    } 
-    else Console.WriteLine("Это не трехзначное чило, попробуйте снова");
+    int y = (x%100)/10;
+    Console.WriteLine ("Вторая цифра " +y);
+    }
+else Console.WriteLine ("Попробуйте снова" );
 }
-SecondNumber ();
+ThirdNumber ();
