d3cb2cb4a0ce68356f3b6bddec85d69352e8203f
Mariia <MK193@yandex.ru>
2022-09-06 19:08:26 +0300

    ex_11Hard done and check

---
+++ HW_2/ex_15/Program.cs
@@ -0,0 +1,18 @@
+﻿//11 hard Напишите программу, которая принимает на вход целое число любой разрядности число и удаляет вторую цифру слева направо этого
+
+
+Console.WriteLine("Введите любое целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+void DelSecondNumber()
+    {
+        int counter = 1;
+        int x2 = x;
+        while (x2>100)
+            {
+            x2 = x2/10;
+            counter = counter*10;
+            }
+        int result = ((x2/10)*counter) + (x%counter);
+        Console.WriteLine("Число без 2й цифры " +result);
+    }
+DelSecondNumber();
