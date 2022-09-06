55a03640c7477ef762a142fc87189afa160d7f8f
Mariia <MK193@yandex.ru>
2022-09-06 18:53:17 +0300

    Ex_13 done and check

---
+++ HW_2/ex_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+gitConsole.WriteLine("Введите число от 1 до 7");
+int x = Convert.ToInt32 (Console.ReadLine());
+void WeekEnd()
+    {
+        if (x>0 & x<8)
+        {
+            if (x==6|x==7) 
+            {
+            Console.WriteLine("Это выходной :)");
+            }
+            else Console.WriteLine("Это рабочий день :(");
+        }
+        else Console.WriteLine("попробуйте ещё раз");
+    }
+    WeekEnd();
