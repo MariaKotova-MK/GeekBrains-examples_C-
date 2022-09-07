c4034a38462b16f1bb611d2aa23c6bd84f01a690
Mariia <MK193@yandex.ru>
2022-09-07 21:40:45 +0300

    Ex_23 done and check

---
+++ HW_3/ex_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+string Cube (int basic)
+{
+string result = string.Empty;
+int count = 1;
+double number;
+while (count <= basic)
+    {
+    number = Math.Pow (count, 3);
+    result = result+number+"  ";
+    count++;
+    }
+return result;
+}
+Console.WriteLine ($"Кубы всех чисел от 1 до заданного числа: {Cube (6)}");
