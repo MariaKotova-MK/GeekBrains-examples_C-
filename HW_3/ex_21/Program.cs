210d1572e46122cf99d5cff89f0addcd71b3ba3f
Mariia <MK193@yandex.ru>
2022-09-07 21:09:50 +0300

    Ex_21 done and check

---
+++ HW_3/ex_21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double distance (int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double res;
+    res = Math.Sqrt ((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za)*(zb - za));
+    return res;
+}
+try 
+            {
+            Console.Write ("Введите координату Х для первой точки ");
+            int xa = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("Введите координату Y для первой точки ");
+            int ya = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("Введите координату Z для первой точки ");
+            int za = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("Введите координату Х для второй точки ");
+            int xb = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("Введите координату Y для второй точки ");
+            int yb = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("Введите координату Z для второй точки ");
+            int zb = Convert.ToInt32 (Console.ReadLine());
+            double rez = distance (xa, ya, za, xb, yb, zb);
+            Console.WriteLine ($"Расстоянием между точкой А с координатами ({xa},{ya},{za}) и точкой В с координатами ({xb},{yb},{zb}) равно {rez}");
+            }
+        catch
+            {
+                Console.WriteLine("please enter integers");
+            }
