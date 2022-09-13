1b0aa7737d0bbe8a15bf143a42b4211fafe372be
Mariia <MK193@yandex.ru>
2022-09-13 11:54:56 +0300

    Ex_25 done ande check

---
+++ HW_4/ex_25/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int factorial(int x, int y)
+{
+int result = 1;
+for (int i = 1; i <= y; i++)
+{
+    result = result * x;
+    }
+    return result;
+}
+try
+{
+    System.Console.Write("Введите число - ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("В какую степень возвести это число - ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine($"{x} в степени {y} = {factorial(x,y)}");
+}
+catch
+{
+    System.Console.WriteLine("Попробуйте снова - Введите именно целое положительное число");
+}
--- test/ex_1/Program.cs
+++ test/ex_1/Program.cs
@@ -1,25 +1,19 @@
-﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 
-double distance (int xa, int ya, int xb, int yb)
+
+// int mult = 1;
+// Console.Write("Введите число - ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int i =1; i<=N; i++) mult = mult*i;
+// Console.Write("Произведение равно " + mult);
+
+
+Console.WriteLine("Введите размерность массива - " );
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+for(int i = 0;i<N;i++)
 {
-    double res;
-    res = Math.Sqrt ((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya));
-    return res;
+        array[i] = new Random().Next(1,100);
+        Console.Write(array[i]+" ");
 }
-try 
-            {
-            Console.Write ("enter x coordinate for first point ");
-            int xa = Convert.ToInt32 (Console.ReadLine());
-            Console.Write ("enter y coordinate for first point ");
-            int ya = Convert.ToInt32 (Console.ReadLine());
-            Console.Write ("enter x coordinate for second point ");
-            int xb = Convert.ToInt32 (Console.ReadLine());
-            Console.Write ("enter y coordinate for second point ");
-            int yb = Convert.ToInt32 (Console.ReadLine());
-            double rez = distance (xa, ya, xb, yb);
-            Console.WriteLine ($"distance between first point with coordinates ({xa},{ya}) and second pont with coordinates ({xb},{yb}) equals {rez}");
-            }
-        catch
-            {
-                Console.WriteLine("please enter integers");
-            }
+
