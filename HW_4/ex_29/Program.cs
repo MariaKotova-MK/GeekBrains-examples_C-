72035bcba1c8e36395d750646581c9c04b3ccd2d
Mariia <MK193@yandex.ru>
2022-09-13 17:34:27 +0300

    ex_29 done and check

---
+++ HW_4/ex_29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран. N - вводится с клавиатуры. Элементы тоже вводятся с клавиатуры.
+
+
+Console.WriteLine("Укажите размер массива - " );
+int count = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[count];
+for (int i = 0; i < count; i++)
+    {
+    Console.WriteLine($"Введите элемент массива с индексом {i} - " ); 
+    mass[i] = Convert.ToInt32(Console.ReadLine()); 
+    }
+Console.WriteLine($"Ваш массив [{String.Join(", ", mass)}]");
+
+
+
