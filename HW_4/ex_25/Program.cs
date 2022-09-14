1a836efacb13d1d5da7ddad397d6a61242adb005
Mariia <MK193@yandex.ru>
2022-09-14 12:24:48 +0300

    Add

--- HW_4/ex_25/Program.cs
+++ HW_4/ex_25/Program.cs
@@ -1,6 +1,6 @@
 ﻿// // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-int factorial(int x, int y)
+int PowerNumber(int x, int y)
 {
 int result = 1;
 for (int i = 1; i <= y; i++)
@@ -15,7 +15,7 @@ try
     int x = Convert.ToInt32(Console.ReadLine());
     System.Console.Write("В какую степень возвести это число - ");
     int y = Convert.ToInt32(Console.ReadLine());
-    System.Console.WriteLine($"{x} в степени {y} = {factorial(x,y)}");
+    System.Console.WriteLine($"{x} в степени {y} = {PowerNumber(x,y)}");
 }
 catch
 {
---
+++ HW_4/ex_27/Program.cs
@@ -0,0 +1,5 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+Double i = StringReader;
+Console.WriteLine (i);
